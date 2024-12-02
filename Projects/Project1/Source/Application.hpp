b23ec010d2ae2e7cdf055199795e00423cba1b28
Im-Bee <Matt.ib.g.12@gmail.com>
2024-12-03 00:56:42 +0100

    ...

--- Projects/Project1/Source/Application.hpp
+++ Projects/Project1/Source/Application.hpp
@@ -2,7 +2,7 @@
 
 #include "Bee/Include/Bee.hpp"
 #include "Bee3D/Include/Bee3D.hpp"
-#include "Bee3D/Include/DX12/Renderer.hpp"
+#include "Bee3D/Include/DX12/RendererDX.hpp"
 #include "Bee/Include/EntryPoint/Win32/Entry.hpp"
 
 class Application : public Bee::App::IApplication
