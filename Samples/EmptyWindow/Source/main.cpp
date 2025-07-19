b5d4c56913a341b03655cc8e94b807f17883ea7f
Matt <144626797+Im-Bee@users.noreply.github.com>
2025-07-19 09:38:45 +0200

    Update main.cpp

--- Samples/EmptyWindow/Source/main.cpp
+++ Samples/EmptyWindow/Source/main.cpp
@@ -6,19 +6,4 @@
 EXPORT_APPLICATION(Application);
 
 void Application::Initialize() 
-{
-    Duckers::EmptyWindow* pWindow = new Duckers::EmptyWindow();
-    Duckers::EmptyWindow* pWindow2 = new Duckers::EmptyWindow();
-
-    pWindow->Create();
-    pWindow->Show();
-
-    pWindow2->Create();
-    pWindow2->Show();
-
-    Duckers::GrowingAllocator<int> growAlloc = { };
-
-    int pArray[5] = { 0, 1, 2, 3, 4 };
-    Duckers::ForEach(pArray, 5, [](auto item) { cout << item << endl; });
-
-}
+{ }
